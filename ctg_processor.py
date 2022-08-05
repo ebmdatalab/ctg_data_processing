07e2e9d6991b627516933cadb5f439d6ea99f8b4
NickCEBM <nicholas.devito@phc.ox.ac.uk>
2022-08-05 13:42:52 +0100

    Initial Prototype
    This is my initial attempt to build a quick and simply script to process the data files we generate for ClinicalTrials.gov. Starting out making this a thing just for me but with some work, this could be made more generalisable.
    
    This rudimentary first version works with turning a single zipped CSV file into a processed CSV. Currently testing multiple files.

---
+++ ctg_processor.py
@@ -0,0 +1,56 @@
+from datetime import date, datetime
+import pandas as pd
+from time import time
+import re
+import os
+from pathlib import Path
+import sys
+from zipfile import ZipFile
+
+from tqdm.auto import tqdm
+
+from lib.data_functions import get_data, fda_reg
+
+#Tests to consider
+#Is the data well formatted?
+#Maybe we should force a date to be in the file name?
+
+#For as long as I'm the primary person using this, we can assume the structure of the file names a build off of that.
+#That is either:
+#clinicaltrials_raw_clincialtrials_json_2022-07-07.csv
+#or
+#clinicaltrials_raw_clincialtrials_json_2022-07-22.csv.zip
+
+#For initial development, I'll use default headers, but could make this cutomisable.
+from lib.final_df import make_dataframe, headers
+
+#Load in the regulatory archive data
+old_fda = 'data/fdaaa_regulatory_snapshot.csv'
+fda_reg_dict = fda_reg(old_fda)
+
+data_path = 'data/data_to_process'
+
+#Get files to process
+files = sorted(os.listdir(data_path))
+
+#Also might be nice to check for the zipped-ness of the file
+#Also, would be nice if we can get a file either locally or via a URL?
+for fi in files:
+    
+    #These steps depend on the file name being the standard format to get the dates
+    
+    if '.zip' in fi:
+        lines = get_data(data_path, fi, zipped=True)
+        sc_date = datetime.strptime(fi.replace('.csv.zip','')[-10:], '%Y-%m-%d').date()
+    else:
+        lines = get_data(data_path, fi)
+        sc_date = datetime.strptime(fi.replace('.csv','')[-10:], '%Y-%m-%d').date()
+    
+    #For now, lets just do it with the act_filter off for testing. Can do fancy things later.
+    #Ideally, I should probably eventually move this to CSV writer as it will be much lower overhead than pandas
+    df = make_dataframe(tqdm(lines), fda_reg_dict, headers, act_filter=False, scrape_date = sc_date)
+    
+    #Will also need to find a way to make this dynamic
+    df.to_csv(f'data/output/ctgov_output_{sc_date}.csv')
+    
+    
---
+++ lib/data_functions.py
@@ -0,0 +1,38 @@
+import csv
+from zipfile import ZipFile
+from requests import get
+from io import BytesIO
+
+def fda_reg(path):
+    """
+    Point this function towards the fdaaa_regulatory_snapshot.csv file to turn it into a
+    dictionary for what you need in this analysis
+    See https://doi.org/10.1101/266452 for more information
+    """
+    fda_reg_dict = {}
+    with open(path) as old_fda_reg:
+        reader = csv.DictReader(old_fda_reg)
+        for d in reader:
+            fda_reg_dict[d['nct_id']] = d['is_fda_regulated']
+    return fda_reg_dict
+
+def get_data(path, file, zipped=False):
+    """
+    Quick function to load the raw ClinicalTrials.gov data which is a CSV of JSON (can think of as ndjson as well)
+    """
+    if zipped:
+        #For now, lets just work with the local case
+        #This assumes a zipped csv file
+        #with ZipFile(BytesIO(get(path, stream=True).content), 'r') as zf:
+        with ZipFile(path + '/' + file, 'r') as zf:
+            #this sting manipulation probably isn't sustainable outside of my useage.
+            with zf.open(file.replace('.zip',''), 'r') as ctgov:
+                lines = ctgov.readlines()
+                return lines
+
+    else:
+        with open(path, 'r') as ctgov:
+            lines = ctgov.readlines()
+            return lines
+    
+        
